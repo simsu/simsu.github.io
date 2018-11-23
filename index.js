5b69e732ecb8dcbb57a37ce1db9162d184d53793
simsu <dvt18@naver.com>
2018-11-23 20:38:43 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -42,23 +42,61 @@ $(document).ready(function() {
 		event.stopPropagation();
 	});
 
-	/*지도*/
-	var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-	    mapOption = { 
-	        center: new daum.maps.LatLng(35.574201396093294, 129.18897732223178), // 지도의 중심좌표
-	        level: 3 // 지도의 확대 레벨
-	    };
+	var leftArrowImg = true;
+	var rightArrowImg = true;
 
-	var map = new daum.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+	$('#leftDir').click(function() {
+		if(imgIndex != 1) {
+			imgIndex--;
 
-	// 마커가 표시될 위치입니다 
-	var markerPosition  = new daum.maps.LatLng(35.574201396093294, 129.18897732223178); 
+			if(imgIndex == 1) {
+				$(this).css('opacity', '0');
+				leftArrowImg = false;
+			}
+		}
+		if(!rightArrowImg) {
+			$('#rightDir').css('opacity', '1');
+			rightArrowImg = true;
+		}
 
-	// 마커를 생성합니다
-	var marker = new daum.maps.Marker({
-	    position: markerPosition
+		$('#showedImg').attr('src','image/picture/' + imgIndex + '.jpg');
+		event.stopPropagation();
+	});
+	$('#rightDir').click(function() {
+		if(imgIndex != 14) {
+			imgIndex++;
+
+			if(imgIndex == 14) {
+				$(this).css('opacity', '0');
+				rightArrowImg = false;
+			}
+		}
+		if(!leftArrowImg) {
+			$('#leftDir').css('opacity', '1');
+			leftArrowImg = true;
+		}
+
+		$('#showedImg').attr('src','image/picture/' + imgIndex + '.jpg');
+		event.stopPropagation();
 	});
 
-	// 마커가 지도 위에 표시되도록 설정합니다
-	marker.setMap(map);
+	/*지도*/
+	// var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+	//     mapOption = { 
+	//         center: new daum.maps.LatLng(35.574201396093294, 129.18897732223178), // 지도의 중심좌표
+	//         level: 3 // 지도의 확대 레벨
+	//     };
+
+	// var map = new daum.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+	// // 마커가 표시될 위치입니다 
+	// var markerPosition  = new daum.maps.LatLng(35.574201396093294, 129.18897732223178); 
+
+	// // 마커를 생성합니다
+	// var marker = new daum.maps.Marker({
+	//     position: markerPosition
+	// });
+
+	// // 마커가 지도 위에 표시되도록 설정합니다
+	// marker.setMap(map);
 });
